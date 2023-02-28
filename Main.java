83739e3ab43a95d223191a05d4c27b88dd1706bf
Joachim Andersson <joaan879@student.liu.se>
2023-02-28 08:35:05 +0100

    Första uppladdning

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import myLogics.*;
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		arg0.setHeight(1000);
+		arg0.setWidth(1500);
+		MainMenu m = new MainMenu(arg0);
+		m.run();
+
+//		Scene mainFrame = new Scene(m);
+//		arg0.setScene(mainFrame);
+//		arg0.show();
+	}
+}
